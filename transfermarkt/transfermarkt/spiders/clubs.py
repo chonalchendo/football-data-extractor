c69232c27bce51ffbdea229a18509a929897d52a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-09-11 18:28:05 +0100

    re-organised the clubs scraper to accommodate all seasons and the top 5 leagues

--- transfermarkt/transfermarkt/spiders/clubs.py
+++ transfermarkt/transfermarkt/spiders/clubs.py
@@ -1,5 +1,6 @@
 import scrapy
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
 
 from ..config.leagues import league_map
 
@@ -11,8 +12,25 @@ class ClubsSpider(scrapy.Spider):
 
     def __init__(self, leagues=None, seasons=None, *args, **kwargs):
         super(ClubsSpider, self).__init__(*args, **kwargs)
-        self.leagues = leagues.split(", ") if leagues and leagues != 'all' else []
-        self.seasons = seasons.split(", ") if seasons and seasons != 'all' else []
+        # self.leagues = leagues.split(", ") if leagues else []
+        # self.seasons = seasons.split(", ") if seasons else []
+        match leagues:
+            case 'all':
+                self.leagues = list(league_map.keys())
+            case leagues if leagues is None:
+                raise ValueError("No leagues provided")
+            case _:
+                self.leagues = leagues.split(", ") 
+
+        match seasons:
+            case 'all':
+                self.seasons = [str(i) for i in range(2018, 2024)]
+            case seasons if seasons is None:
+                raise ValueError("No seasons provided")
+            case _:
+               self.seasons = seasons.split(", ") 
+
+
 
     def parse(self, response):
         """
@@ -20,13 +38,25 @@ class ClubsSpider(scrapy.Spider):
 
         @url https://transfermarkt.co.uk/premier-league/startseite/wettbewerb/GB1/plus/?saison_id=2018
         @returns items 1
-        @scrapes team_name team_id
+        @scrapes league season team_name team_id
         """
         soup = BeautifulSoup(response.text, 'html.parser') 
         team_info = soup.find_all("td", {"class": "hauptlink no-border-links"})
         team_name = [td.find("a").get("href").split("/")[1] for td in team_info]
         team_id = [td.find("a").get("href").split("/")[4] for td in team_info]
-        yield {"team_name": team_name, "team_id": team_id}
+
+
+        # get league and season from the url
+        url = response.url
+        league = urlparse(url).path.split("/")[1]
+        season = parse_qs(urlparse(url).query)['saison_id'][0]
+
+        yield {
+            'league': league, 
+            'season': season, 
+            "team_name": team_name, 
+            "team_id": team_id
+        }
 
 
 
@@ -35,24 +65,25 @@ class ClubsSpider(scrapy.Spider):
         Start the requests for the given leagues and seasons.
         @returns request 1
         """
-        match self.leagues:
-            case 'all':
-                self.leagues = list(league_map.keys())
-            case self.leagues if len(self.leagues) == 0:
-                raise ValueError("No leagues provided")
-            case _:
-                pass
-
-        match self.seasons:
-            case 'all':
-                self.seasons = [str(i) for i in range(2018, 2024)]
-            case self.seasons if len(self.seasons) == 0:
-                raise ValueError("No seasons provided")
-            case _:
-                pass
+        # match self.leagues:
+        #     case 'all':
+        #         self.leagues = list(league_map.keys())
+        #     case self.leagues if len(self.leagues) == 0:
+        #         raise ValueError("No leagues provided")
+        #     case _:
+        #         pass
+        #
+        # match self.seasons:
+        #     case 'all':
+        #         self.seasons = [str(i) for i in range(2018, 2024)]
+        #     case self.seasons if len(self.seasons) == 0:
+        #         raise ValueError("No seasons provided")
+        #     case _:
+        #         pass
 
         for season in self.seasons:
             for league in self.leagues:
+
                 url = self.base_url.format(
                     league=league, 
                     league_id=league_map.get(league), 

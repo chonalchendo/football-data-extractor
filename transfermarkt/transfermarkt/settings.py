3ffcfd465b6d840c3c1561641196a8b01e4790bb
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-09-11 18:28:55 +0100

    added feeds to the scraper

--- transfermarkt/transfermarkt/settings.py
+++ transfermarkt/transfermarkt/settings.py
@@ -91,3 +91,10 @@ COOKIES_ENABLED = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+FEEDS = {
+    'data/%(name)s.json.gz': {
+        'format': 'jsonlines',
+        'encoding': 'utf-8',
+        'overwrite': True,
+    }
+}

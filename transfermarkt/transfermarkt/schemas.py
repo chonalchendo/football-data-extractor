af04ce50abdafc084ab96a591c36bd3ff4c68261
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-09 15:14:08 +0100

    Pydantic for validation of each item

---
+++ transfermarkt/transfermarkt/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class Player(BaseModel):
+    name: str
+    dob: str
+    age: int
+    country: str
+    current_club: str | None
+    height: str
+    position: str
+    value: str
+    joined_date: str | None
+    number: str
+    signed_from: str | None
+    signing_fee: str
+    tm_id: str
+    tm_name: str
+    foot: str
+    season: int 
+    squad: str
+

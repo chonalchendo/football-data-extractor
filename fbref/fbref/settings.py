9e788f0b7f43fd4b98b57a106145839ae6d1c936
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-19 16:57:03 +0100

    created a settings class to store scraper settings

---
+++ fbref/fbref/settings.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Settings:
+    def setdict(self, settings: dict) -> None:
+        for key, value in settings.items():
+            self.set(key, value)
+
+    def set(self, key: str, value: str) -> None:
+        setattr(self, key, value)
+    

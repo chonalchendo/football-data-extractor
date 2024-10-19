50fdcdc1e20897e1afe9a03dcdb263fad3138d8b
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-19 19:41:21 +0100

    commented out example code

--- fbref/fbref/collectors/player_defense.py
+++ fbref/fbref/collectors/player_defense.py
@@ -36,26 +36,26 @@ class PlayerDefense(BasePlayerCollector):
             valid_record = DefenseStats(**record)
             yield valid_record.model_dump()
 
-if __name__ == "__main__":
-
-    filepath = "fbref/data/defense/2021-2022/defense.json.gz"
-
-    print(Path(__file__).resolve())
-
-    if not Path(filepath).parent.exists():
-        Path(filepath).parent.mkdir(parents=True, exist_ok=True)
-
-    defense = PlayerDefense(season="2021-2022")
-    for i, record in enumerate(defense.collect()):
-
-        if i == 0:
-            file = gzip.open(filepath, "wt")
-            file.write(json.dumps(record))
-
-        else:
-            file = gzip.open(filepath, "at")
-            file.write("\n")
-            file.write(json.dumps(record))
-
-    print("Done")
-
+# if __name__ == "__main__":
+#
+#     filepath = "fbref/data/defense/2021-2022/defense.json.gz"
+#
+#     print(Path(__file__).resolve())
+#
+#     if not Path(filepath).parent.exists():
+#         Path(filepath).parent.mkdir(parents=True, exist_ok=True)
+#
+#     defense = PlayerDefense(season="2021-2022")
+#     for i, record in enumerate(defense.collect()):
+#
+#         if i == 0:
+#             file = gzip.open(filepath, "wt")
+#             file.write(json.dumps(record))
+#
+#         else:
+#             file = gzip.open(filepath, "at")
+#             file.write("\n")
+#             file.write(json.dumps(record))
+#
+#     print("Done")
+#

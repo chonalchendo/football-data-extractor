7d786a50e020b72daea2481502f9c200e10f9d90
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 18:55:27 +0100

    validator schema for gca stats

---
+++ fbref/fbref/schemas/_player_gca.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class GcaStats(BaseModel):
+    rk: str
+    player: str
+    nation: str | None
+    pos: str
+    squad: str
+    comp: str
+    age: str
+    born: str
+    ninety_mins_played: str
+    shot_creating_actions: str
+    sca_per_90: str
+    pass_live_sca: str
+    pass_dead_sca: str
+    take_on_sca: str
+    shot_sca: str
+    foul_drawn_sca: str
+    defensive_action_sca: str
+    goal_creating_actions: str
+    gca_per_90: str
+    pass_live_gca: str
+    pass_dead_gca: str
+    take_on_gca: str
+    shot_gca: str
+    foul_drawn_gca: str
+    defensive_action_gca: str

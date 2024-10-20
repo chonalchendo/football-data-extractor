95fe3064ac3e336a22254a02924bd7228737a970
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 18:55:15 +0100

    validator schema for defense stats

---
+++ fbref/fbref/schemas/_player_defense.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class DefenseStats(BaseModel):
+    rk: str
+    player: str
+    nation: str | None
+    squad: str
+    comp: str
+    age: str
+    born: str
+    position: str
+    ninety_mins_played: str
+    tackles: str
+    tackles_won: str
+    def_third_tackles: str
+    mid_third_tackles: str
+    att_third_tackles: str
+    dribblers_tackled: str
+    dribblers_challenged: str
+    tackle_pct: str | None
+    challenges_lost: str
+    blocks: str
+    shots_blocked: str
+    passes_blocked: str
+    interceptions: str
+    tackles_interceptions: str
+    clearances: str
+    errors: str

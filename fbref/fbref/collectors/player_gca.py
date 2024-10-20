265b7e293ef8d48b1eb5fd8cc820018e3af815fc
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 18:46:48 +0100

    collector for gca stats

--- fbref/fbref/collectors/player_gca.py
+++ fbref/fbref/collectors/player_gca.py
@@ -1,13 +1,8 @@
 from ..cleaners.common import common_cleaners
-from ..cleaners.gca import gca_cleaners 
-from ..schemas import GcaStats 
+from ..cleaners.gca import gca_cleaners
+from ..schemas import GcaStats
 from .base import BasePlayerCollector
 
-
-PlayerGca = BasePlayerCollector(
-    stat='gca',
-    table='gca',
-    validator=GcaStats,
-    cleaners=common_cleaners + gca_cleaners
+gca_collector = BasePlayerCollector(
+    stat="gca", table="gca", validator=GcaStats, cleaners=common_cleaners + gca_cleaners
 )
-

5427b5ecb7edb29e0ebe618ce53307eba9e8d0c2
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 18:54:58 +0100

    initiate schema package

---
+++ fbref/fbref/schemas/__init__.py
@@ -0,0 +1,25 @@
+from ._player_defense import DefenseStats
+from ._player_gca import GcaStats
+from ._player_keeper import KeeperStats
+from ._player_keeper_adv import KeeperAdvStats
+from ._player_misc import MiscStats
+from ._player_passing import PassingStats
+from ._player_passing_types import PassingTypeStats
+from ._player_playing_time import PlayingTimeStats
+from ._player_possession import PossessionStats
+from ._player_shooting import ShootingStats
+from ._player_standard_stats import StandardStats
+
+__all__ = [
+    "DefenseStats",
+    "GcaStats",
+    "KeeperStats",
+    "KeeperAdvStats",
+    "MiscStats",
+    "PassingStats",
+    "PassingTypeStats",
+    "PlayingTimeStats",
+    "PossessionStats",
+    "ShootingStats",
+    "StandardStats",
+]

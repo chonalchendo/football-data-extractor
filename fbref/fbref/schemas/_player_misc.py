ab5071a22b166842a6c979a2b89d82199a1c497d
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 18:56:10 +0100

    validator schema for miscellanous stats

---
+++ fbref/fbref/schemas/_player_misc.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class MiscStats(BaseModel):
+    rk: str
+    player: str
+    nation: str
+    pos: str
+    squad: str
+    comp: str
+    age: str
+    born: str
+    ninety_mins_played: str
+    yellow_cards: str
+    red_cards: str
+    second_yellow_cards: str
+    fouls: str
+    fouls_drawn: str
+    offsides: str
+    crosses: str
+    interceptions: str
+    tackles_won: str
+    penalties_won: str
+    penalties_conceded: str
+    own_goals: str
+    ball_recoveries: str
+    aerials_won: str
+    aerials_lost: str
+    aerials_won_pct: str

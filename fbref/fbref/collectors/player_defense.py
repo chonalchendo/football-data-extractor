29855cd8975e6fa40df5db55718d1f8c4e116b61
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-20 10:06:35 +0100

    deleted commented out code

--- fbref/fbref/collectors/player_defense.py
+++ fbref/fbref/collectors/player_defense.py
@@ -1,15 +1,16 @@
-import polars as pl
 from functools import reduce
-from typing import Iterator 
-import gzip
-import json
-from pathlib import Path
+from typing import Iterator
 
-from .base import BasePlayerCollector
+import polars as pl
+
+from utils.logger import get_logger
 
 from ..cleaners.common import common_cleaners
-from ..cleaners.defense import defense_cleaners 
+from ..cleaners.defense import defense_cleaners
 from ..schemas import DefenseStats
+from .base import BasePlayerCollector
+
+logger = get_logger(__name__)
 
 
 class PlayerDefense(BasePlayerCollector):
@@ -21,41 +22,17 @@ class PlayerDefense(BasePlayerCollector):
         )
         self.cleaners = common_cleaners + defense_cleaners
 
-
-    def collect(self) -> Iterator[dict]:  
+    def collect(self) -> Iterator[dict]:
         data = self.parse()
-        data2 = reduce(
-            lambda data, cleaner: cleaner(data), self.cleaners, data
-        )
+        data2 = reduce(lambda data, cleaner: cleaner(data), self.cleaners, data)
         data3 = data2.with_columns(
-            pl.Series('season', [self.season] * len(data2)),
+            pl.Series("season", [self.season] * len(data2)),
         )
 
+        if data3.shape[0] == 0:
+            logger.error(f"No data found for {self.stat} in {self.season}")
+            raise ValueError
 
         for record in data3.to_dicts():
             valid_record = DefenseStats(**record)
             yield valid_record.model_dump()
-
-# if __name__ == "__main__":
-#
-#     filepath = "fbref/data/defense/2021-2022/defense.json.gz"
-#
-#     print(Path(__file__).resolve())
-#
-#     if not Path(filepath).parent.exists():
-#         Path(filepath).parent.mkdir(parents=True, exist_ok=True)
-#
-#     defense = PlayerDefense(season="2021-2022")
-#     for i, record in enumerate(defense.collect()):
-#
-#         if i == 0:
-#             file = gzip.open(filepath, "wt")
-#             file.write(json.dumps(record))
-#
-#         else:
-#             file = gzip.open(filepath, "at")
-#             file.write("\n")
-#             file.write(json.dumps(record))
-#
-#     print("Done")
-#

2f19bb31b26466894499a329faf8bd64ea087fc4
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-22 09:05:05 +0100

    put passing rename columns function in a list

--- fbref/fbref/collectors/player_passing.py
+++ fbref/fbref/collectors/player_passing.py
@@ -7,5 +7,5 @@ passing_collector = BasePlayerCollector(
     stat="passing",
     table="passing",
     validator=PassingStats,
-    cleaners=common_plus_xg_cleaners + passing_rename_cols,
+    cleaners=common_plus_xg_cleaners + [passing_rename_cols],
 )

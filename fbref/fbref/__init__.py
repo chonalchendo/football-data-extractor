daccd7d2c80c943298ea13e7eeec9caaa086714a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-11 20:36:08 +0000

    added parquet feed

--- fbref/fbref/__init__.py
+++ fbref/fbref/__init__.py
@@ -1,5 +1,5 @@
-from .feeds import GcsFeed
+from .feeds import GcsFeed, ParquetFeed
 from .navigator import NavigatorRunner
 from .settings import Settings
 
-__all__ = ["NavigatorRunner", "Settings", "GcsFeed"]
+__all__ = ["NavigatorRunner", "Settings", "GcsFeed", "ParquetFeed"]

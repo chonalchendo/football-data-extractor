d8cd25eba239be4b0f36f885b8b4202dac60fdcf
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-17 18:54:04 +0000

    added urls to scrape

---
+++ fbref/fbref/constants.py
@@ -0,0 +1,2 @@
+PLAYER_STATS_URL = "https://fbref.com/en/comps/Big5/{season}/{stat}/players/{season}-Big-5-European-Leagues-Stats#stats_{table}"
+PLAYER_WAGES_URL = "https://fbref.com/en/comps/{comp_id}/{season}/wages/{season}-{comp_name}-Wages#player_wages"

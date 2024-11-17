08a2a31d3fde8c37b92e06482fd82a234a66b412
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-17 18:58:49 +0000

    added parameters to be passed to base collector

--- fbref/fbref/collectors/base.py
+++ fbref/fbref/collectors/base.py
@@ -1,6 +1,6 @@
 from collections.abc import Sequence
 from functools import reduce
-from typing import Callable, Iterator, TypeAlias
+from typing import Callable, Iterator, TypeAlias, TypedDict
 
 import pandas as pd
 import polars as pl
@@ -14,24 +14,38 @@ logger = get_logger(__name__)
 DataFrameTransform: TypeAlias = Callable[[pl.DataFrame], pl.DataFrame]
 
 
+class Params(TypedDict):
+    season: str | None = None
+
+
+class StatsParams(Params):
+    stat: str
+    table: str
+
+
+class WageParams(Params):
+    comp_id: str
+    comp_name: str
+
+
 class BasePlayerCollector:
     def __init__(
         self,
-        stat: str,
-        table: str,
         validator: BaseModel,
-        season: str | None = None,
+        url: str,
+        url_params: Params,
+        season: str | None = None,  # set later
         cleaners: Sequence[DataFrameTransform] = (),
     ) -> None:
         self.season = season
-        self.stat = stat
-        self.table = table
         self.validator = validator
         self.cleaners = cleaners
-        self.url = "https://fbref.com/en/comps/Big5/{season}/{stat}/players/{season}-Big-5-European-Leagues-Stats#stats_{table}"
+        self.url_params = url_params
+        self.url = url
 
     def collect(self) -> Iterator[dict]:
         data = self.parse()
+
         data2 = reduce(lambda data, cleaner: cleaner(data), self.cleaners, data)
         data3 = data2.with_columns(
             pl.Series("season", [self.season] * len(data2)),
@@ -44,7 +58,7 @@ class BasePlayerCollector:
         print(data3.columns)
 
         for record in data3.to_dicts():
-            valid_record = self.validator(**record)
+            valid_record: BaseModel = self.validator(**record)
             yield valid_record.model_dump()
 
     def parse(self) -> pl.DataFrame:
@@ -52,9 +66,15 @@ class BasePlayerCollector:
         season = f"{self.season}-{str(int(self.season) + 1)}"
         logger.info(f"Collecting data for {season}")
 
-        params = {"season": season, "stat": self.stat, "table": self.table}
-        url = self.url.format(**params)
+        self.url_params["season"] = season
+
+        url = self.url.format(**self.url_params)
 
         logger.info(f"Collecting data from {url}")
+        
+        if 'wages' in url:
+            df_pandas = pd.read_html(url)[1]
+            return pl.from_pandas(df_pandas)
+        
         df_pandas = pd.read_html(url, skiprows=1, header=0)[0]
         return pl.from_pandas(df_pandas)

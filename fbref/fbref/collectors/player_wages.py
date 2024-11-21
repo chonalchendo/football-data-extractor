627f772f6a7a3590da6e04716ff5cf79ebef83b2
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-21 21:29:55 +0000

    added renaming function to cleaners

--- fbref/fbref/collectors/player_wages.py
+++ fbref/fbref/collectors/player_wages.py
@@ -1,4 +1,5 @@
 from ..cleaners.common import wage_cleaners
+from ..cleaners.wages import wages_rename_cols
 from ..schemas import WageStats
 from .base import BasePlayerCollector, WageParams
 from ..constants import PLAYER_WAGES_URL
@@ -9,5 +10,5 @@ def wage_collector(comp_id: str, comp_name: str) -> BasePlayerCollector:
         url=PLAYER_WAGES_URL,
         url_params=WageParams(comp_id=comp_id, comp_name=comp_name),
         validator=WageStats,
-        cleaners=wage_cleaners,
+        cleaners=wage_cleaners + [wages_rename_cols],
     )

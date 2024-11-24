9968fd9fbfd4ad92651e3d2548b6544d6d03c7bd
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-24 16:10:57 +0000

    created stats index based on year

---
+++ transfermarkt/transfermarkt/config/parser_indexes.py
@@ -0,0 +1,23 @@
+INDEXES = {
+    "2024": {
+        "current_club": None,
+        "numbers": 0,
+        "ages": 1,
+        "countries": 2,
+        "height": 3,
+        "foot": 4,
+        "joined_date": 5,
+        "signing_info": 6,
+        'contract_expiry': 7,
+    },
+    "pre-2024": {
+        "numbers": 0,
+        "ages": 1,
+        "countries": 2,
+        "current_club": 3,
+        "height": 4,
+        "foot": 5,
+        "joined_date": 6,
+        "signing_info": 7,
+    },
+}

4c81962d9424563e2312b3f494a69c51ba613823
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-26 20:32:16 +0000

    removed whitespace

--- fbref/fbref/constants.py
+++ fbref/fbref/constants.py
@@ -1,2 +1,4 @@
 PLAYER_STATS_URL = "https://fbref.com/en/comps/Big5/{season}/{stat}/players/{season}-Big-5-European-Leagues-Stats#stats_{table}"
 PLAYER_WAGES_URL = "https://fbref.com/en/comps/{comp_id}/{season}/wages/{season}-{comp_name}-Wages#player_wages"
+
+

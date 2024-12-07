10f54632d729f485ee2be99e3f3b9434cf907374
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-12-07 13:42:21 +0000

    changed import to fully absolute

--- fbref/fbref/feeds.py
+++ fbref/fbref/feeds.py
@@ -5,7 +5,9 @@ from pathlib import Path
 import gcsfs
 import polars as pl
 
-from utils.logger import get_logger
+# from utils.logger import get_logger
+from fbref.utils.logger import get_logger
+
 
 logger = get_logger(__name__)
 
